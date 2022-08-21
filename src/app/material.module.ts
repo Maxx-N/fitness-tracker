d133a5008a2a6d816f57ae99d4f465c3ea6f6a17
maximenorval <maximenorval@gmail.com>
2022-08-21 16:38:29 +0200

    40. Working on the Sidenav and Toolbar

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const materialModules = [
   MatButtonModule,
@@ -17,6 +18,7 @@ const materialModules = [
   MatNativeDateModule,
   MatCheckboxModule,
   MatSidenavModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
