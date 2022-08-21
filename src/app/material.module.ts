963a399831cee346d6168e4ca7a945a6a22bcddc
maximenorval <maximenorval@gmail.com>
2022-08-21 19:47:00 +0200

    49. Adding a Spinner to the Training Screen

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -12,6 +12,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const materialModules = [
   MatButtonModule,
@@ -27,6 +28,7 @@ const materialModules = [
   MatTabsModule,
   MatCardModule,
   MatSelectModule,
+  MatProgressSpinnerModule,
 ];
 
 @NgModule({
