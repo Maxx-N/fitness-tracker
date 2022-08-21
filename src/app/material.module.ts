552cc9bd5979ce039ef69f42d51b58f0d34bc816
maximenorval <maximenorval@gmail.com>
2022-08-21 11:39:47 +0200

    28. Flexbox - A Quick Refresher

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+const materialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule],
+  imports: materialModules,
+  exports: materialModules,
 })
 export class MaterialModule {}
