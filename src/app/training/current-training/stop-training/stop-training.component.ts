39bf0fde4eaa99b26c8bd0367de5f09f156159ae
maximenorval <maximenorval@gmail.com>
2022-08-21 21:36:57 +0200

    52. Passing Data to the Dialog

--- src/app/training/current-training/stop-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training/stop-training.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-stop-training',
   templateUrl: './stop-training.component.html',
-  styleUrls: ['./stop-training.component.scss']
+  styleUrls: ['./stop-training.component.scss'],
 })
 export class StopTrainingComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
