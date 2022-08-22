6c662ad3cc13ab51522ca3b97e2fc51d4e41e707
maximenorval <maximenorval@gmail.com>
2022-08-22 06:35:44 +0200

    Assignment 2: Time to Practice - Angular Material & Data

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,18 +1,44 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() sidenavClosed = new EventEmitter();
+  isAuth: boolean;
+  private authsubscription: Subscription;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authsubscription = this.authService.authChange.subscribe(
+      (result: boolean) => {
+        this.isAuth = result;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authsubscription.unsubscribe();
+  }
 
   onCloseSidenav() {
+    this.sidenavClosed.emit();
+  }
+
+  onLogout(): void {
+    this.authService.logout();
     this.sidenavClosed.emit();
   }
 }
