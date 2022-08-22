0496441f8096ff634926cfdbd62bdd03016939da
maximenorval <maximenorval@gmail.com>
2022-08-22 10:24:16 +0200

    75. Adding Pagination to the Data Table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -16,6 +16,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 const materialModules = [
   MatButtonModule,
@@ -35,6 +36,7 @@ const materialModules = [
   MatDialogModule,
   MatTableModule,
   MatSortModule,
+  MatPaginatorModule,
 ];
 
 @NgModule({
