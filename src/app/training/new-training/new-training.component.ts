f4629d9d420418a73f62d55cbd20213802dc7b2f
maximenorval <maximenorval@gmail.com>
2022-08-23 07:38:24 +0200

    84. Listening to Value Changes (of Firestore)

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -10,13 +12,19 @@ import { TrainingService } from '../training.service';
   styleUrls: ['./new-training.component.scss'],
 })
 export class NewTrainingComponent implements OnInit {
-  availableExercises: Exercise[] = [];
+  availableExercises: Observable<any[]>;
   selectedExerciseId: string;
 
-  constructor(private trainingService: TrainingService) {}
+  constructor(
+    private trainingService: TrainingService,
+    private db: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
-    this.availableExercises = this.trainingService.getAvailableExercises();
+    // this.availableExercises = this.trainingService.getAvailableExercises();
+    this.availableExercises = this.db
+      .collection('availableExercises')
+      .valueChanges();
   }
 
   onStartTraining(form: NgForm) {
