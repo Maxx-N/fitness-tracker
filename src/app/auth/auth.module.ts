b86800aa53847c1017640184187e01abd5ca5edd
maximenorval <maximenorval@gmail.com>
2022-08-24 08:52:19 +0200

    108. Splitting Up Routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '../shared/shared.module';
 
+import { SharedModule } from '../shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [SharedModule, ReactiveFormsModule],
+  imports: [SharedModule, ReactiveFormsModule, AuthRoutingModule],
   exports: [],
 })
 export class AuthModule {}
